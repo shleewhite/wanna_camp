223e7ac6d48ed53880f81ba23e34e10d578d9b82
Lee <leewhite128@gmail.com>
2016-11-12 11:07:11 -0800

    trying to add database

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+
 
 import './main.html';
 
 Template.submit.events({
-	'submit form': function(){
-		SearchDB
-	}
+	
 });
