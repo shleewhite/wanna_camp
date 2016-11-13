1cd9e67955f2ac7eb7dd72a2ee23e7782b49d718
Lee <leewhite128@gmail.com>
2016-11-12 23:13:19 -0800

    small steps

--- client/main.js
+++ client/main.js
@@ -2,21 +2,26 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './main.html';
-import '../server/main.js'
+// import '../server/main.js'
 
-Meteor.subscribe("places");
+Meteor.subscribe("CampSites");
 
 if (Meteor.isClient) {
     Template.submit.events({
 		'submit #subButton': function( event ){
         	event.preventDefault();
 	        var month =	document.getElementById("months").value;
-	        var fDistance = document.getElementById("distance").value;
+	        var distance = document.getElementById("distance").value;
 	        var water = document.getElementById("water").value;
 	        var bathroom = document.getElementById("bathroom").value;
 	        var cook = document.getElementById("cook").value;
-	        console.log(month, distance, water, bathroom, cook);
-	        console.log(CampSites.find({ distance: { $lte: fDistance} } ));
+	        var arr = CampSites.find().fetch();
+
+	        for(int i = 0; i < arr.length; i++) {
+	        	if (arr[i].distance <= distance) {
+	        		
+	        }
+	        console.log(arr[0].distance);
     	}
     });
 }
