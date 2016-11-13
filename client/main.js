9e4c42ed85ad4e89639fbdc71c25cb3af2a57a1c
Lee <leewhite128@gmail.com>
2016-11-12 22:28:16 -0800

    progress on query and form matching

--- client/main.js
+++ client/main.js
@@ -2,7 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './main.html';
+import '../server/main.js'
 
-// Template.submit.events({
+Meteor.subscribe("places");
 
-// });
+if (Meteor.isClient) {
+    Template.submit.events({
+		'submit #subButton': function( event ){
+        	event.preventDefault();
+	        var month =	document.getElementById("months").value;
+	        var fDistance = document.getElementById("distance").value;
+	        var water = document.getElementById("water").value;
+	        var bathroom = document.getElementById("bathroom").value;
+	        var cook = document.getElementById("cook").value;
+	        console.log(month, distance, water, bathroom, cook);
+	        console.log(CampSites.find({ distance: { $lte: fDistance} } ));
+    	}
+    });
+}
