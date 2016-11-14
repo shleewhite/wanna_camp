581c015dbf9121b8410c4662bff99f1566fd305d
Lee <leewhite128@gmail.com>
2016-11-14 12:50:28 -0800

    started writing function to add result js

--- client/main.js
+++ client/main.js
@@ -8,7 +8,6 @@ Meteor.subscribe("CampSites");
 if (Meteor.isClient) {
     Template.submit.events({
 		'submit #subButton': function( event ){
-			var results;
         	event.preventDefault();
 	        var month =	document.getElementById("months").value;
 	        var distance = document.getElementById("distance").value;
@@ -16,6 +15,8 @@ if (Meteor.isClient) {
 	        var bathroom = document.getElementById("bathroom").value;
 	        var cook = document.getElementById("cook").value;
 	        var arr = CampSites.find().fetch();
+	        var results = document.getElementById("results");
+	        var num = 0;
 
 	        document.getElementById("subButton").style.display = "none";
 
@@ -25,17 +26,27 @@ if (Meteor.isClient) {
 						if( (arr[i].water == true && water == "Yes") || (arr[i].water == false && water == "No")) {
 							if( (arr[i].plumbing == true && bathroom == "Yes") || (arr[i].plumbing == false && bathroom == "No")) {
 								if( (arr[i].cooking == true && cook == "Yes") || (arr[i].cooking == false && cook == "No")) {
-									console.log(arr[i]);
-									results += arr[i].name;
-
+									num += 1;
+									results.innerHTML += resHTML(num, arr[i].name, arr[i].pet, arr[i].water, arr[i].plumbing, arr[i].distance, arr[i].nearby, arr[i].reservation, arr[i].cooking);
 								}
 							}
 						}
 					}
 				}
 			}
-			document.getElementById("results").innerHTML += results;
-			document.getElementById("results").innerHTML += "<br><a href='/'>Begin New Search</a>"
+			document.getElementById("refreshButton").style.display = "block";
+
+			function resHTML (num, name, pet, water, bathroom, distance, nearby, reservation, fire) {
+				var result;
+				result = "<div id='result" + num + "'><p>"+ name + "</p>"
+						  + "<div id='lists'><ul id='list1'><li>Pets Allowed: " + pet + "</li>"
+						  + "<li>Fires Allowed: " + fire + "</li>"
+						  + "<li>Water on Site: " + water + "</li>"
+						  + "<li>Bathrooms on Site: " + bathroom + "</li>"
+						  + "<li>Distance from UCSC Campus: " + distance + " miles</li></ul>"
+						  + "</div></div>";
+				return result;
+			}
     	}
     });
 }
