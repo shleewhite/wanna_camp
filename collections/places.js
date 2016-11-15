7d6dbc7494b37f5ef494605feda9e79fb284e629
Lee <leewhite128@gmail.com>
2016-11-14 21:16:11 -0800

    database added to file

--- collections/places.js
+++ collections/places.js
@@ -27,7 +27,7 @@ let PlacesSchema = new SimpleSchema({
   },
   'reservation': {
   	type: String,
-  	label: 'Link to reserve campsite if required.'
+  	label: 'Link to reserve campsite if necessary.'
   },
   'nearby': {
   	type: [String],
@@ -40,3 +40,11 @@ let PlacesSchema = new SimpleSchema({
 });
 
 CampSites.attachSchema( PlacesSchema );
+
+CampSites.insert({name: "Henry Cowell Campground", distance: 10, closed: ["November", "December", "January", "February"], water: true, plumbing: true, cooking: true, reservation: "http://www.reserveamerica.com/camping/henry-cowell-redwoods-sp/r/facilityDetails.do?contractCode=CA&parkId=120041", nearby: ["Redwoods", "30 miles of trails", "fishing"], pet: true});
+CampSites.insert({name: "Bob's Pine Grove", distance: 4, closed: [], water: true, plumbing: true, cooking: false, reservation: "null", nearby: ["Along Big Trees Railroad Tracks", "Borders base of Pogonip"], pet: true});
+CampSites.insert({name: "Big Basin Redwoods State Park", distance: 30, closed: [], water: true, plumbing: true, cooking: true, reservation: "http://www.reserveamerica.com/camping/big-basin-redwoods-sp/r/campgroundDetails.do?contractCode=CA&parkId=120009", nearby: ["Swimming", "Surfing", "Bike Trails"], pet: false});
+CampSites.insert({name: "Sunset State Beach", distance: 20, closed: [], water: true, plumbing: true, cooking: false, reservation: "http://www.reserveamerica.com/camping/sunset-sb/r/campgroundDetails.do?contractCode=CA&parkId=120094", nearby: ["Swimming", "Surfing"], pet: false});
+CampSites.insert({name: "Laguna Seca Recreation Area", distance: 53, closed: [], water: true, plumbing: true, cooking: true, reservation: "null", nearby: ["Views of Salinas Valley", "Hiking Trails"], pet: false});
+CampSites.insert({name: "Pfieffer Big Sur State Park", distance: 75, closed: [], water: true, plumbing: false, cooking: true, reservation: "http://www.reserveamerica.com/camping/pfeiffer-big-sur-sp/r/campgroundDetails.do?contractCode=CA&parkId=120068", nearby: ["Forested", "Big Sur River"], pet: false});
+CampSites.insert({name: "Uvas Canyon County Park", distance: 47, closed: [], water: true, plumbing: true, cooking: true, reservation: "https://gooutsideandplay.org/", nearby: ["Waterfall Hike", "Picnic Spots"], pet: true});
